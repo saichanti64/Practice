0379ed80a6a53bdab22b6b8886e8bbfd2bd8dbaf
sai <sai.chanti64@gmail.com>
2024-06-07 18:26:49 +0530

    Initial commit

---
+++ src/main/java/com/example/Practice/PracticeController/PracController.java
@@ -0,0 +1,17 @@
+package com.example.Practice.PracticeController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prac")
+public class PracController {
+
+    @GetMapping("/getPrac")
+    public String getDetails(){
+        return "Getting Practice Details";
+    }
+
+}
